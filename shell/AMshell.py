50d195576abc62319c5e8cb547288d6041b3d395
AndreasMaeurer <amaeurer@miners.utep.edu>
2018-09-17 20:21:50 -0600

    fixed the cd without argument but; also fixed empty string input bug

--- shell/AMshell.py
+++ shell/AMshell.py
@@ -8,6 +8,12 @@ Theory of OS Fall 2018
 Collaborated:	I discussed redirection with Stephanie Medina
 				With Sergio Ponce De Leon I discussed how to set up the shell to print to the File Descriptor
 """
+"""
+exit and cd work
+Should a help function be implemented?
+if so, what info should it print out to the user?
+If it's not required, I'm leaving it out.
+"""
 
 import sys
 import os
@@ -16,7 +22,7 @@ import re
 
 shellInput = ""
 
-# function to launch the commands.  taken mostly from p3-exec.py
+# Method to launch the commands.  Taken mostly from p3-exec.py
 def launch(args):
 	pid = os.getpid()
 	os.write(1, ("About to fork (pid:%d)\n" % pid).encode())		#for debugging
@@ -27,46 +33,30 @@ def launch(args):
 		sys.exit(1)
 		
 	elif rc == 0:                   # child
-		#os.write(1, ("Child: My pid==%d.  Parent's pid=%d\n" % (os.getpid(), pid)).encode())		#for debugging
-		#args = ["wc", "AMshell.py"]
+		
 		for dir in re.split(":", os.environ['PATH']): # try each directory in the path
 			program = "%s/%s" % (dir, args[0])
-			#os.write(1, ("Child:  ...trying to exec %s\n" % program).encode())
 			try:
 				os.execve(program, args, os.environ) 	# try to exec program
 			except FileNotFoundError:             		# ...expected
 				pass                              		# ...fail quietly
-
-		#os.write(2, ("Child:    Could not exec %s\n" % args[0]).encode())							#for debugging
 		sys.exit(1)                 # terminate with error
 
-	else:                           # parent (forked ok)
-		#os.write(1, ("Parent: My pid=%d.  Child's pid=%d\n" % (pid, rc)).encode())					#for debugging
+	else:                           # parent (forked ok)		
 		childPidCode = os.wait()
-		#os.write(1, ("Parent: Child %d terminated with exit code %d\n" % childPidCode).encode())	#for debugging
-
+				
 """
 cd (change directory) Can not change the directory of it's parent if it's a separate program.  So it has to be implemented as a function of the shell
 adapted from: https://brennan.io/2015/01/16/write-a-shell-in-c/
 It checks for errors, and returns
 """
+# Method to change directory
 def myCD(args):
-	if (args[1] == None):							#check if the parameter exists [or should that be done below?]
-		print("Correct usage: cd <argument> ")		#print error message if it doesn't exist
-	else:		
-		#print("What is parameter at this point: " + parameter)	#for debugging		#TODO: consider deleting this line
-		(os.chdir(args[1])) 						#then call chdir(path)
-		
-"""
-exit and cd work
-Should a help function be implemented?
-if so, what info should it print out to the user?
-If it's not required, I'm leaving it out.
-"""
-
-"""
-BUG:  When you hit enter without any input the Shell exits (CRASHES!)
-"""
+	if (args[0] == "cd"): 			#check if the parameter is "cd" (If we write no path (only 'cd'), then return out of this method)
+		return 1	
+	else:							# Else we change the directory to the one specified by the argument, (if possible)
+		(os.chdir(args[1])) 		#then call chdir(path)
+		return 0
 
 # The main loop of the shell.
 while (True):
@@ -78,45 +68,13 @@ while (True):
 	userInput = shellInput.split()				#Split a string into a list where each word is a list item
 	print("user Input is: ")
 	print(userInput)							#for debugging		#TODO: consider deleting this line
-	# os.write(1, ("%s\n" % userInput).encode())  # for debugging	#1 is the File Descriptor, right?  #TODO: consider deleting this line
-	
-	#validate(userInput)
-	
-	
-	# before you execute any code check if the arguments match the built in methods
-	# if yes run the built in methods here before the lauch method is reached.
-	if (userInput == None):
-		print("yes this is what is happening")
-		continue	
-	else:					
-		if ((len(userInput) > 1) & (userInput[0] == "cd")):
-			# BUG BUG BUG: Maybe I should check at this point if the Parameter exits?
-			if (userInput[1] == None):							#check if the parameter exists						#BUG BUG BUG: When you type in cd by itself it crashes here.
-				print("Correct usage: cd <argument> ")
-			#print("this line has been reached") #for debugging		#TODO: consider deleting this line
-			else:
-				myCD(userInput)
-		else:
-			status = launch(userInput)
-			
-	"""
-	
-	if (len(userInput) > 0):
-		if (userInput[0] == "cd"):
-			# BUG BUG BUG: Maybe I should check at this point if the Parameter exits?
-			if (userInput[1] == None):							#check if the parameter exists						#BUG BUG BUG: When you type in cd by itself it crashes here.
-				print("Correct usage: cd <argument> ")
-			#print("this line has been reached") #for debugging		#TODO: consider deleting this line
-			else:
-				myCD(userInput[1])
-	else:
-		status = launch(userInput)
-	"""
-	
-	"""
-	if (userInput == None):
-		print("yes this LInE HerE Is bEIng rEAcheD")
+		
+	# hit enter without any input the Shell continues in the infinite loop, without issues.
+	if not userInput:
 		continue
-	else:
+	
+	if (userInput[0] != "cd"):
 		status = launch(userInput)
-	"""
+	else:
+		myCD(userInput)
+		
